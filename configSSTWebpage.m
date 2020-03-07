d1fa0412c945655b8918425931fe15001d9d8c48
Pedro Velez <pvelezbelchi@gmail.com>
2020-03-07 23:31:45 +0000

    forced rename files

---
+++ configSSTWebpage.m
@@ -0,0 +1,12 @@
+load Globales
+
+
+DirDataSST=strcat(GlobalSU.DatPath,'/Satelite/noaa.oisst.v2.highres');
+FileDataSST=strcat(DirDataSST,'/NOAAOisstv2HighresSstDayMean');
+
+DirFigures=strcat(GlobalSU.ProPath,'/SSTWebpage');
+EstRaprocan=strcat(GlobalSU.ProPath,'/SSTWebpage/data/EstacionesRaprocan.txt');
+EstNorteTenerife=strcat(GlobalSU.ProPath,'/SSTWebpage/data/EstacionesNorteTenerife.txt');
+
+NAnosR=4; %years to represent
+
