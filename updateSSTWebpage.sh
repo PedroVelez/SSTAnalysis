31c048aafdc00c590189837b6c93f575c5919dd0
Pedro Velez <pvelezbelchi@gmail.com>
2020-07-22 20:04:07 +0100

    updated bot

--- updateSSTWebpage.sh
+++ updateSSTWebpage.sh
@@ -90,8 +90,8 @@ fi
 
 
 #------------------------------------
-# Anual and Ciclo estacional y Hovmoller plots
+# i TelegramBot
 #------------------------------------
 URL="https://api.telegram.org/bot$ArgoEsBotTOKEN/sendMessage"
-MENSAJE=`cat /Users/pvb/Dropbox/Oceanografia/Proyectos/SSTWebpage/data/report.txt`
+MENSAJE=`cat $HOME/Dropbox/Oceanografia/Proyectos/SSTWebpage/data/report.txt`
 curl -s -X POST $URL -d chat_id=$ArgoEsBotID -d text="$MENSAJE"
