e86cf13d7204467322bd1b2b8f4e205b01f13ecc
Pedro Velez <pvelezbelchi@gmail.com>
2023-05-03 15:23:34 +0000

    Update figures

--- updateSSTWebpage.sh
+++ updateSSTWebpage.sh
@@ -98,6 +98,13 @@ fi
 # i TelegramBot
 #------------------------------------
 URL="https://api.telegram.org/bot$ArgoEsBotTOKEN/sendMessage"
+URLimg="https://api.telegram.org/bot$ArgoEsBotTOKEN/sendphoto?chat_id=$ArgoEsChannel"
 MENSAJE=`cat $HOME/Proyectos/SSTWebpage/data/report.txt`
+
 curl -s -X POST $URL -d chat_id=$ArgoEsChannel -d text="$MENSAJE" -d parse_mode=html > $DirLog/bot.log
+curl -F "photo=@$HOME/Proyectos/SSTWebpage/images/GraficosSSTNorteTenerife_CicloEstacional_Seccion01_02.png" $URLimg -F caption="GraficosSSTNorteTenerife_CicloEstacional"
+curl -F "photo=@$HOME/Proyectos/SSTWebpage/images/GraficosSSTNorteTenerife_Anual_Seccion01_02.png" $URLimg -F caption="GraficosSSTNorteTenerife_Anual"
+curl -F "photo=@$HOME/Proyectos/SSTWebpage/images/GraficosSSTNorteTenerife_Mensual_Seccion01_02.png" $URLimg -F caption="GraficosSSTNorteTenerife_Mensual"
+
+
 
